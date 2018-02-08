11028ee0f1abf3da9507b3ceb4c8e33bf9bacb97
Tony Wong <leung_sun@hotmail.com>
2018-02-08 15:22:38 +0100

    first push

---
+++ phoneword/phoneword/IDialer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phoneword
+{
+    namespace Phoneword
+    {
+        public interface IDialer
+        {
+            bool Dial(string number);
+        }
+    }
+}
